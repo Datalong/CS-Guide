8671bc7522f28dc372751d9171a107813594aacf
Datalong <2922722656@qq.com>
2021-06-26 23:32:33 +0800

    Test2

---
+++ src/jichu01/BasicSyntax/variable/Account_Customer/Test2.java
@@ -0,0 +1,7 @@
+package jichu01.BasicSyntax.variable.Account_Customer;/**
+* @Auther: Datalong
+  @Date: 2021/6/26 - 06 -26 -21:12
+  @Dscription: jichu01.BasicSyntax.variable.Account_Customer
+  @version: 1.0
+*/   public class Test2 {
+}
